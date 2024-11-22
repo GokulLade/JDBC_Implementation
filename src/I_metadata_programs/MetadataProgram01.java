2d8f1228accd4215d283921b94ed44a17ed4e353
Gokul <gokullade89@gmail.com>
2024-11-22 22:57:58 +0530

    Updated Metadata Program

--- src/I_metadata_programs/MetadataProgram01.java
+++ src/I_metadata_programs/MetadataProgram01.java
@@ -43,8 +43,6 @@ public class MetadataProgram01 {
 				System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t"+rs.getFloat(3)+"\t"+rs.getString(4));
 			}
 			
-			
-			
 		}
 		catch(Exception e)
 		{
